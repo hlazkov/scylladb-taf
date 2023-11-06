9fdca2a639a3857a5b6ce5e1e31ebacaebf5300a
phlazkov <phlazkov@star.global>
2023-11-06 18:58:19 +0200

    fix: cleanup

--- src/pageObjects/BasePage.ts
+++ src/pageObjects/BasePage.ts
@@ -9,11 +9,6 @@ export class BasePage {
     this.page = page;
   }
 
-  // async below added to show the function returns a promise
-  async getUrl() {
-    return this.page.url();
-  }
-
   async navigate() {
     await this.page.goto(this.url);
   }
